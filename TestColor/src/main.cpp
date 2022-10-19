24e69d984768b07474d754fdf7e23c1a5cefdabe
redouanedebart <redouane.debart@gmail.com>
2022-10-19 16:17:12 +0200

    warning corrected

--- TestColor/src/main.cpp
+++ TestColor/src/main.cpp
@@ -1,17 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "Movement.h"
-#include "Tracking.h"
 #include "Preprocessing.h"
+#include "Tracking.h"
 
 
 int main(){
     
     long nrh,nrl,nch,ncl;
 	rgb8 **I;
-	byte **R;
-	float sum=0;
-	int i,j;
+
 	int tab[2];
 	I=LoadPPM_rgb8matrix("droite.ppm",&nrl,&nrh,&ncl,&nch);
 	getRedBarycenter(I, nrl, nrh, ncl, nch, tab);
